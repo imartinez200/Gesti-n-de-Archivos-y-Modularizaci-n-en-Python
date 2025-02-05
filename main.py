4e0bad4eae3b0c853f72ee9102225d85077a13ca
imartinez200 <chaco0527@gmail.com>
2025-02-04 20:59:11 -0600

    proyecto modulo

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 # Descripción: Este archivo contiene el punto de entrada principal del proyecto.
 # Recuerda incluir tu nombre completo, la fecha en la que iniciaste el proyecto y la fecha estimada de entrega.
 # Esto ayuda a mantener un registro claro del trabajo realizado.
+
+
+from analisis_datos.estadisticas  import media,calcular_mediana
+
+lista_parametro = [5,3,1,2,4]
+
+print('Media: ', media( lista_parametro))
+print('Mediana: ', calcular_mediana(lista_parametro))
